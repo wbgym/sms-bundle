86b9c6eabeb0b16f50480743ea6a4ad7ac2ef2f8
Markus <mmi.github@t-online.de>
2020-04-11 10:52:33 +0200

    corrected spelling (html-unicode)

--- src/Resources/contao/languages/de/modules.php
+++ src/Resources/contao/languages/de/modules.php
@@ -15,7 +15,7 @@ $GLOBALS['TL_LANG']['MOD']['sms']					= 'Schüler machen Schule';
 $GLOBALS['TL_LANG']['MOD']['sms_courses']			= array('SMS Kurse', 'Mit diesem Modul lassen sich SMS Kurse erstellen und verwalten.');
 $GLOBALS['TL_LANG']['MOD']['sms_courses_wishes']	= array('SMS Zuteilungen', 'Mit diesem Modul lassen sich fest zugeteilte Schüler anzeigen und neue Schüler einem Kurs fest zuteilen.');
 $GLOBALS['TL_LANG']['MOD']['sms_exchange']	= array('SMS Tauschbörse', 'Mit diesem Modul lassen sich Kurswechselwünsche der SmS-Tauschbörse verwalten.');
-$GLOBALS['TL_LANG']['MOD']['sms_choice_admin']		= array('SMS Verwaltung&nbsp;&amp;&nbsp;Stats', 'Mit diesem Modul lassen sich Aktion zur Verwaltung und Auswertung der Kurswahl durchfüheren.');
+$GLOBALS['TL_LANG']['MOD']['sms_choice_admin']		= array('SMS Verwaltung', 'Mit diesem Modul lassen sich Aktion zur Verwaltung und Auswertung der Kurswahl durchfüheren.');
 
 $GLOBALS['TL_LANG']['FMD']['sms']					= 'Schüler machen Schule';
 $GLOBALS['TL_LANG']['FMD']['wb_sms_course_choice']	= array('SMS Kurswahl', 'Listet alle Kurse der SMS Woche auf, eingeloggte Schüler können ihren Kurs wählen.');
